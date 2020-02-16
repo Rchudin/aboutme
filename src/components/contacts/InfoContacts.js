231bfa800d2a627007ebe2c06eca02339fa51b0d
rchudin <me@ruslanchudin.ru>
2020-02-16 17:07:48 +0300

    Update InfoContacts.js

--- src/components/contacts/InfoContacts.js
+++ src/components/contacts/InfoContacts.js
@@ -5,7 +5,7 @@ import {useTranslation} from "react-i18next";
 import {NavLink} from "react-router-dom";
 
 
-const email = "rus.chudin@gmail.com";
+const email = "me@ruslanchudin.ru";
 const telegram = "t.me/rchudin";
 
 
