aac75cdb62700203a40bf5c8519b6eeaa82e12be
rchudin <me@ruslanchudin.com>
2020-03-02 13:52:08 +0300

    webp

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import {NavLink, Route, Switch} from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
+import Img from "react-webp-image";
 import s from './App.module.css';
 import Navbar from "../navbar/Navbar";
-import background from '../../assets/img/mads.png'
+import backgroundImg from '../../assets/img/mads.png'
+import backgroundWebp from "../../assets/img/mads.webp"
 import logo from "../../assets/img/RC.png"
 import routes from "../../utils/routes";
 
 
-export default ({token, isInit}) => {
+export default ({ token, isInit }) => {
 
     return (
         <>
             <div className={s.app}>
                 <div className={s.content}>
                     <NavLink to="/" className="app_header">
-                        <img src={logo} alt="logo"/>
+                        <img src={logo} alt="logo" />
                     </NavLink>
                     <Switch>
                         {routes.map((route, i) => (
@@ -23,20 +25,20 @@ export default ({token, isInit}) => {
                                 path={route.path}
                                 exact={route.exact}
                                 render={props =>
-                                    <route.component routes={route.routes} token={token} isInit={isInit}/>
+                                    <route.component routes={route.routes} token={token} isInit={isInit} />
                                 }
                             />
                         ))}
                     </Switch>
                 </div>
                 <div className={s.navigation}>
-                    <Navbar/>
+                    <Navbar />
                 </div>
             </div>
             <Route path='/' exact={true}>
-                <img src={background} alt="" className={s.app_img_background}/>
+                <Img src={backgroundImg} webp={backgroundWebp} className={s.app_img_background} alt="" />;
+              {/* <img src={backgroundWebp} alt="" className={s.app_img_background}/> */}
             </Route>
         </>
     )
 }
-
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(png|svg|jpg|gif|ico|txt)$/,
+                test: /\.(png|svg|jpg|gif|ico|txt|webp)$/,
                 loader: 'file-loader',
                 options: {
                     outputPath: 'assets',
